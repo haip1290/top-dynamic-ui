bbfb6aff68fdd5bdec855228fc923d6e5262a2cc
Hai Pham <thanh_hai1290@yahoo.com>
2025-02-03 17:26:23 +0700

    Add dropdown using transition

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ btn.addEventListener("click", () => {
   const menu = document.querySelector("#dropdown");
   menu.classList.toggle("show");
 });
+
+document.addEventListener("click", (event) => {
+  const isDropdownBtn = event.target.matches("[data-dropdown-button]");
+  const currentDropdown = event.target.closest("[data-dropdown]");
+  if (!isDropdownBtn && currentDropdown != null) return;
+  if (isDropdownBtn) {
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
